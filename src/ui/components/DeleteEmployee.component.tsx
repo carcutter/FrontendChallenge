3ba7bba30c72f74e87ef497f2f3cf64495e4ae7e
Sandro Baccega <mail.sandro.baccega@gmail.com>
2025-01-30 10:45:45 +0100

    feat: ✨ Add alert dialog to delete button

--- src/ui/components/DeleteEmployee.component.tsx
+++ src/ui/components/DeleteEmployee.component.tsx
@@ -3,6 +3,17 @@ import { EmployeeIdModel } from "@/domain/models/employee.model";
 import { Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog";
 import { Button } from "./button";
 
 export interface DeleteEmployeeProps {
@@ -25,9 +36,26 @@ const DeleteEmployee = ({ id, fullWidth }: DeleteEmployeeProps): ReactNode => {
   }
 
   return (
-    <Button disabled={isPending} onClick={onClick} className="w-fit flex gap-2">
-      <Trash /> {fullWidth ? "Delete Employee" : null}
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button disabled={isPending} className="w-fit flex gap-2">
+          <Trash /> {fullWidth ? "Delete Employee" : null}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the
+            employee.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onClick}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 export default DeleteEmployee;
